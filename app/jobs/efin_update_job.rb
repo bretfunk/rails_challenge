0ff92c4616ff259ad88017dfafed1e8bc9c26258
BFunk <bret.e.funk@gmail.com>
2018-01-03 15:38:10 -0500

    added comments, pulled out the service, still stuck on formatting the post request, no idea, will keep plugging

--- app/jobs/efin_update_job.rb
+++ app/jobs/efin_update_job.rb
@@ -2,14 +2,10 @@ class EfinUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(data)
-    #byebug
     test_data = {
-        "household": 1,
-          "income":  20000
+      "household": 1,
+      "income":  20000
     }
-
-    conn = Faraday.new(url: "http://efin.oddball.io")
-    byebug
-    conn.post "/", test_data
+    OddballService.post_request(test_data)
   end
 end
---
+++ app/services/oddball_service.rb
@@ -0,0 +1,24 @@
+class OddballService
+  attr_reader :conn, :data
+  def initialize(data)
+    @data = data
+    @conn = Faraday.new(url: "http://efin.oddball.io") do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+    def self.post_request(data)
+      new(data).post_request
+    end
+
+    def post_request
+      byebug
+      parse(@conn.post "/", data)
+    end
+
+    def parse(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+end
